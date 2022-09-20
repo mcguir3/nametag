8af4c11dc614969e1aa7da906d6f1b89b54b6121
mcguir3 <112960699+mcguir3@users.noreply.github.com>
2022-09-20 13:37:21 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
 basic.forever(function () {
-	
+    basic.showString("ryker")
+    basic.showString("once lived in a tall place")
+    basic.showString("now he doesnt")
+    basic.showString("the end :)")
 })
